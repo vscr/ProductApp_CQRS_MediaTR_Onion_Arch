47d9a548e8b7b630169d9692d0ca2c3c52579c33
vscr <vsecer1@gmail.com>
2023-04-19 21:40:37 +0300

    fluent validator

---
+++ src/WebApi/ProductApp.WebApi/Operations/Abstract/IProductOperations.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApp.Application.Dto;
+using ProductApp.Application.Features.Commands;
+using ProductApp.Application.Wrappers;
+using System.Threading.Tasks;
+
+namespace ProductApp.WebApi.Operations.Abstract
+{
+    public interface IProductOperations
+    {
+        Task<ServiceResponse<ProductDto>> Post(CreateProductCommand command);
+        Task<IActionResult> Get()
+    }
+}
---
+++ src/WebApi/ProductApp.WebApi/Validation/ProductValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using ProductApp.Application.Dto;
+using ProductApp.Application.Features.Commands;
+
+namespace ProductApp.WebApi.Validation
+{
+    public class ProductValidator: AbstractValidator<CreateProductCommand>
+    {
+        public ProductValidator()
+        {
+            RuleFor(product => product.Name).NotNull().NotEmpty();
+            RuleFor(product => product.Value).NotNull().NotEqual(0);
+            RuleFor(product => product.Quantity).NotNull().NotEqual(0);
+        }
+    }
+}
